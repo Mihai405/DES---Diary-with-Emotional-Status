8867afe096a643b32a7f5e6782d53b8643928d34
ralucacigmaian <ralucacigmaian@yahoo.com>
2024-05-21 18:30:11 +0300

    Sign Up & Sign In Screens

---
+++ DES-mobile-application/src/utils/colors.js
@@ -0,0 +1,10 @@
+export const Colors = {
+  white: "#fff",
+  primaryColor: "#051d38",
+  secondaryColor: "#f1f8fb",
+  tertiaryColor: "#b3d4f4",
+  lightBlue: "#88c3f1",
+  darkBlue: "#43b0f3",
+  purple: "#814f89",
+  pink: "#f69097",
+};
