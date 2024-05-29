eac0fdd8d73ae91a469168986039e6bda7847822
Sebastian Iscruleasa <sebastian.iscruleasa@visma.com>
2024-05-30 01:01:59 +0300

    improved mood card

--- web/src/components/History/MoodCard.tsx
+++ web/src/components/History/MoodCard.tsx
@@ -2,6 +2,7 @@ import { MoodData } from '../../types';
 import { moods } from '../MoodForm/MoodButtons';
 import { Button } from '@mui/material';
 import './styles.css';
+import { capitalizeFirstLetter } from '../../utils';
 
 export function MoodCard({ moodData }: { moodData: MoodData }) {
     return (
@@ -9,7 +10,7 @@ export function MoodCard({ moodData }: { moodData: MoodData }) {
             <div className="moodCard-header">
                 <div className="moodCard-mood">
                     <div style={{ fontSize: '36px' }}>
-                        {moods[moodData.mood]}
+                        {moods[capitalizeFirstLetter(moodData.mood)]}
                     </div>
                     <b style={{ fontSize: '24px' }}>{moodData.mood}</b>
                 </div>
@@ -18,6 +19,9 @@ export function MoodCard({ moodData }: { moodData: MoodData }) {
                 >
                     Delete
                 </Button>
+            </div>
+            <div style={{ fontSize: '14px', marginBottom: '8px' }}>
+                {moodData.date.toString().split('T')[0]}
             </div>
             <div style={{ fontSize: '18px' }}>
                 Because of <b>{moodData.reason}</b>
